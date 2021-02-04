b9cdeed729ed163738833ad75e4c02362853b4f0
benjamminf <benjfleming21@gmail.com>
2021-02-04 11:37:23 +1100

    More unit tests for createFactory

--- src/tests/createFactory.test.ts
+++ src/tests/createFactory.test.ts
@@ -1,5 +1,6 @@
 import bucket from '../bucket';
 import createFactory from '../createFactory';
+import createState from '../createState';
 import {mockFn} from './testUtils';
 
 describe('createFactory()', () => {
@@ -88,20 +89,118 @@ describe('createFactory()', () => {
       expect(factory(2)).not.toBe(item2);
     });
 
-    test('should update keys whenever value is added', () => {});
+    test('should update keys whenever value is added', () => {
+      const factory = createFactory((key: number) => `item${key}`);
+      const observer = mockFn();
+      factory.keys.observe(observer);
+      factory.set(
+        new Map([
+          [1, 'new1'],
+          [2, 'new2'],
+        ])
+      );
+      expect(observer).toBeCalledTimes(1);
+      expect(factory.keys.get()).toEqual(new Set([1, 2]));
+    });
 
-    test('should update keys whenever value is deleted', () => {});
+    test('should update keys whenever value is deleted', () => {
+      const factory = createFactory((key: number) => `item${key}`);
+      const observer = mockFn();
+      factory(1);
+      factory(2);
+      factory.keys.observe(observer);
+      factory.set(new Map([[1, 'new1']]));
+      expect(observer).toBeCalledTimes(1);
+      expect(factory.keys.get()).toEqual(new Set([1]));
+    });
 
-    test('should not update keys whenever value is updated', () => {});
+    test('should not update keys whenever value is updated', () => {
+      const factory = createFactory((key: number) => `item${key}`);
+      const observer = mockFn();
+      factory(1);
+      factory(2);
+      factory.keys.observe(observer);
+      factory.set(
+        new Map([
+          [1, 'new1'],
+          [2, 'new2'],
+        ])
+      );
+      expect(observer).not.toBeCalled();
+      expect(factory.keys.get()).toEqual(new Set([1, 2]));
+    });
   });
 
   describe('when bounding', () => {
-    test('should create item when inside key range', () => {});
+    test('should create item when inside key range function', () => {
+      const range = (key: number) => key > 0;
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(1)).not.toThrowError();
+      expect(() => factory(2)).not.toThrowError();
+      expect(() => factory(3)).not.toThrowError();
+    });
 
-    test('should throw error when creating item outside key range', () => {});
+    test('should throw error when creating item outside key range function', () => {
+      const range = (key: number) => key > 0;
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(0)).toThrowError();
+      expect(() => factory(-1)).toThrowError();
+      expect(() => factory(-2)).toThrowError();
+    });
 
-    test('should update keys when restricting key range after item creation', () => {});
+    test('should create item when inside key range iterable', () => {
+      const range = [1, 2, 3];
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(1)).not.toThrowError();
+      expect(() => factory(2)).not.toThrowError();
+      expect(() => factory(3)).not.toThrowError();
+    });
 
-    test('should update values when restricting key range after item creation', () => {});
+    test('should throw error when creating item outside key range iterable', () => {
+      const range = [1, 2, 3];
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(0)).toThrowError();
+      expect(() => factory(-1)).toThrowError();
+      expect(() => factory(4)).toThrowError();
+    });
+
+    test('should create item when inside key range selector', () => {
+      const range = createState([1, 2, 3]);
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(1)).not.toThrowError();
+      expect(() => factory(2)).not.toThrowError();
+      expect(() => factory(3)).not.toThrowError();
+    });
+
+    test('should throw error when creating item outside key range selector', () => {
+      const range = createState([1, 2, 3]);
+      const factory = createFactory((key: number) => `item${key}`, range);
+      expect(() => factory(0)).toThrowError();
+      expect(() => factory(-1)).toThrowError();
+      expect(() => factory(4)).toThrowError();
+    });
+
+    test('should update keys with key range selector after item creation', () => {
+      const range = createState([1, 2, 3]);
+      const factory = createFactory((key: number) => `item${key}`, range);
+      factory(1);
+      factory(2);
+      factory(3);
+      range.set([1, 2]);
+      expect(factory.keys.get()).toEqual(new Set([1, 2]));
+    });
+
+    test('should update values with key range selector after item creation', () => {
+      const range = createState([1, 2, 3]);
+      const factory = createFactory((key: number) => `item${key}`, range);
+      const item1 = factory(1);
+      const item2 = factory(2);
+      factory(3);
+      range.set([1, 2]);
+      const values = factory.get();
+      expect(values.get(1)).toBe(item1);
+      expect(values.get(2)).toBe(item2);
+      expect(values.has(3)).toBeFalsy();
+    });
   });
 });
