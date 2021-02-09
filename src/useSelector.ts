c4aa3fe9c58eb64aa51bcbea28ff1d962ac59090
benjamminf <benjfleming21@gmail.com>
2021-02-09 20:24:43 +1100

    Fix incorrect naming

--- src/useSelector.ts
+++ src/useSelector.ts
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Selector} from './createSelector';
 
-export default function useSharedSelector<T>(sharedSelector: Selector<T>): T {
+export default function useSelector<T>(sharedSelector: Selector<T>): T {
   const value = sharedSelector.get();
   const [, setObserveCounter] = useState(0);
 
